9faf1fa8fb9b8375b40e77f8e5fb29ac21afb6e0
Kevin Yulias <wb-kevinyuli532085@antfin.com>
2019-07-07 23:50:54 +0700

    Added tabs in home

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -8,18 +8,44 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Transactions List"),
-      ),
-      body: BlocProvider(
-        builder: (context) => TransactionsListBloc(),
-        child: TransactionsList(),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => {},
-        label: Text("Scan"),
-      ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Transactions List"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.directions_car)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            BlocProvider(
+              builder: (context) => TransactionsListBloc(),
+              child: TransactionsList()
+            ),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () => {},
+          label: Text("Scan"),
+        ),
+      )
     );
   }
 }
+
+// BlocProvider(
+          // builder: (context) => TransactionsListBloc(),
+          // child: TabBarView(
+          //     children: [
+          //       Icon(Icons.directions_car),
+          //       Icon(Icons.directions_transit),
+          //       Icon(Icons.directions_bike),
+          //     ],
+          //   ),
